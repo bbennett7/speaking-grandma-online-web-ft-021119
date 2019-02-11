f544005cc2ba5d4c25caaf5131b570301bcea8b4
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-11 23:29:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 
 def speak_to_grandma(phrase)
   if phrase.upcase != phrase
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase == phrase
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   end
 end
